abb5c85a81543edf84711eda0b9eaa62cd9fc6d4
snowmerak <snowmerak@outlook.com>
2023-01-07 17:00:15 +0900

    update sample code

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/snowmerak/eureka/token"
 )
 
 func main() {
-	a := []byte(`abc + def+789`)
+	a := []byte(`select * from table where id = 1 and name = "snowmerak"`)
+	fmt.Println([]byte(` = 1 and name = "snowmerak"`))
 
 	tokens, err := token.Parse(a)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
 	for _, t := range tokens {
